340c1f5bd6e30e9a2c3c9facc9a9b26b90695fbc
Juhi22 <j.dani22@freemail.hu>
2019-11-15 22:00:44 +0100

    Create CartController interface

--- server/src/main/java/homeproject/webshop/webshop/controller/CartControllerImpl.java
+++ server/src/main/java/homeproject/webshop/webshop/controller/CartControllerImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/cart")
-public class CartController {
+public class CartControllerImpl implements CartController {
 
     @Autowired
     private CartService cartService;

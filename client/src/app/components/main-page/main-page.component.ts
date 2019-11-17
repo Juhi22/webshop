6aa3fa06f79376dff9b523aa3c297754f47e7c68
micsikz <micsikzoltan@vipmail.hu>
2019-11-17 10:45:35 +0100

    product methods use product service

--- client/src/app/components/main-page/main-page.component.ts
+++ client/src/app/components/main-page/main-page.component.ts
@@ -13,34 +13,27 @@ export class MainPageComponent implements OnInit {
   constructor(private http: HttpClient,
               private productServ: ProductService) {}
 
-  //baseUrl: string = 'http://192.168.0.50:8050';
     baseUrl: string = 'http://192.168.1.4:8050';
 
-  productName: string = '';
-  productPrice: number = 0;
   products: Product[] = [];
-  product: Product;
+  product = {} as Product;
 
   ngOnInit() {
     this.getProducts();
   }
 
   getProducts() {
-    //this.http.get<Product[]>(this.baseUrl + '/product/getAllProducts')
     this.productServ.getProducts()
     .subscribe(data => this.products = data);
   }
 
   addProduct() {
-    this.product = {
-      name: this.productName,
-      price: this.productPrice
-    };
-    this.productName = '';
-    this.productPrice = 0;
     this.http.post<Product>(this.baseUrl + '/product/addProduct', this.product)
-    .subscribe(data => this.getProducts());
+    .subscribe(data => this.getProducts()
+    );
     this.getProducts();
+    this.product.name = '';
+    this.product.price = 0;
   }
 
   isEmpty() {

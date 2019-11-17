f0fa241018e1920d5a5fa31c5e170d338f868dec
Juhi22 <j.dani22@freemail.hu>
2019-11-17 16:33:25 +0100

    Add lombok

--- server/src/main/java/homeproject/webshop/webshop/domain/Product.java
+++ server/src/main/java/homeproject/webshop/webshop/domain/Product.java
@@ -1,11 +1,14 @@
 package homeproject.webshop.webshop.domain;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.List;
 import java.util.Set;
 
 @Entity
 @Table(name = "product")
+@Data
 public class Product {
 
     @Id
@@ -26,78 +29,16 @@ public class Product {
     private ProductCategories category;
 
     @Column
-    @ElementCollection(targetClass=Integer.class)
+    @ElementCollection(targetClass = Integer.class)
     private List<Integer> ratings;
 
     @Column
     private Double rating;
 
+    @Column
+    @ElementCollection(targetClass = String.class)
+    private List<String> comments;
+
     @ManyToMany(mappedBy = "products")
     Set<Cart> carts;
-
-    public Product(){}
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    public ProductCategories getCategory() {
-        return category;
-    }
-
-    public void setCategory(ProductCategories category) {
-        this.category = category;
-    }
-
-    public List<Integer> getRatings() {
-        return ratings;
-    }
-
-    public void setRatings(List<Integer> ratings) {
-        this.ratings = ratings;
-    }
-
-    public Double getRating() {
-        return rating;
-    }
-
-    public void setRating(Double rating) {
-        this.rating = rating;
-    }
-
-    public Set<Cart> getCarts() {
-        return carts;
-    }
-
-    public void setCarts(Set<Cart> carts) {
-        this.carts = carts;
-    }
 }

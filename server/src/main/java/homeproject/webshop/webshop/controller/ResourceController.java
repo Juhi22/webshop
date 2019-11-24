78aed0c1ab0862312567a6aec2efff6f23c43713
Juhi22 <j.dani22@freemail.hu>
2019-11-24 15:20:33 +0100

    Add uncompleted config and controller

---
+++ server/src/main/java/homeproject/webshop/webshop/controller/ResourceController.java
@@ -0,0 +1,29 @@
+package homeproject.webshop.webshop.controller;
+
+import homeproject.webshop.webshop.domain.WebShopUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/springjwt")
+public class ResourceController {
+    @Autowired
+    private GenericService userService;
+
+    @RequestMapping(value ="/cities")
+    @PreAuthorize("hasAuthority('ADMIN_USER') or hasAuthority('STANDARD_USER')")
+    public List<RandomCity> getUser(){
+        return userService.findAllRandomCities();
+    }
+
+    @RequestMapping(value ="/users", method = RequestMethod.GET)
+    @PreAuthorize("hasAuthority('ADMIN_USER')")
+    public List<WebShopUser> getUsers(){
+        return userService.findAllUsers();
+    }
+}

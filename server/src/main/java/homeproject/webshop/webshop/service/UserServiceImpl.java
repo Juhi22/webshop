ba41ba2d658fb7e3b7f9500b6e9ad6a3f01b4d9b
Juhi22 <j.dani22@freemail.hu>
2019-12-08 16:19:18 +0100

    Import List

--- server/src/main/java/homeproject/webshop/webshop/service/UserServiceImpl.java
+++ server/src/main/java/homeproject/webshop/webshop/service/UserServiceImpl.java
@@ -5,6 +5,8 @@ import homeproject.webshop.webshop.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserServiceImpl implements UserService{
 

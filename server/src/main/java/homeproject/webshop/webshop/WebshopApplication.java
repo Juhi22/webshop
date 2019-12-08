cfae34b77afd3cbc5f8241da31c4faf3bf372a33
Juhi22 <j.dani22@freemail.hu>
2019-12-08 17:25:29 +0100

    Configure Swagger

--- server/src/main/java/homeproject/webshop/webshop/WebshopApplication.java
+++ server/src/main/java/homeproject/webshop/webshop/WebshopApplication.java
@@ -9,6 +9,10 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
@@ -26,6 +30,16 @@ public class WebshopApplication {
 	public CommandLineRunner getCommandLineRunner(ApplicationContext context) {
 		servlet.setThrowExceptionIfNoHandlerFound(true);
 		return args -> {};
+	}
+
+	//configuration bean for Swagger UI
+	@Bean
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.any())
+				.apis(RequestHandlerSelectors.basePackage("homeproject.webshop"))
+				.build();
 	}
 
 }

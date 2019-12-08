34a61e558314da597bb5fad29663fcec7ea74880
Juhi22 <47113900+Juhi22@users.noreply.github.com>
2019-12-08 18:56:32 +0100

    Feature/add swagger (#6)
    * Configure Swagger
    
    * Configure Swagger

--- server/src/main/java/homeproject/webshop/webshop/WebshopApplication.java
+++ server/src/main/java/homeproject/webshop/webshop/WebshopApplication.java
@@ -9,9 +9,15 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-@EnableWebMvc
+//@EnableWebMvc
+@EnableSwagger2
 public class WebshopApplication {
 	@Autowired
 	private DispatcherServlet servlet;
@@ -24,6 +30,16 @@ public class WebshopApplication {
 	public CommandLineRunner getCommandLineRunner(ApplicationContext context) {
 		servlet.setThrowExceptionIfNoHandlerFound(true);
 		return args -> {};
+	}
+
+	//configuration bean for Swagger UI
+	@Bean
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.any())
+				.apis(RequestHandlerSelectors.basePackage("homeproject.webshop"))
+				.build();
 	}
 
 }
--- server/src/main/java/homeproject/webshop/webshop/exception/PageNotFound.java
+++ server/src/main/java/homeproject/webshop/webshop/exception/PageNotFound.java
@@ -2,7 +2,6 @@ package homeproject.webshop.webshop.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
